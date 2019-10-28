3e3de713c844689bc09902cc64b7e7b39ecb782b
Connor Clark <Conclarkh5@gmail.com>
2019-10-28 12:20:11 -0700

    edits in class

---
+++ javascript-tests/primitives.js
@@ -0,0 +1,22 @@
+const myString = "Learning Javascript in class.";
+const myFirstNumericVariable = 3;
+const myBoolean = true;
+const myNull = null;
+let myUndefined;
+
+var oldStyle = "This is the way we used to do it.";
+let newChangableStyle = "This allows you to declare a variable that can be changed.";
+const constantVariable = "This variable can not change.";
+
+const quotable = 'He said, "Hello."';
+
+console.log(myString);
+console.log('TYPE:', typeof myString);
+console.log(myFirstNumericVariable);
+console.log('TYPE:', typeof myFirstNumericVariable);
+console.log(myBoolean);
+console.log('TYPE:', typeof myBoolean);
+console.log(myNull);
+console.log('TYPE:', typeof myNull);
+console.log(myUndefined);
+console.log('TYPE:', typeof myUndefined);
