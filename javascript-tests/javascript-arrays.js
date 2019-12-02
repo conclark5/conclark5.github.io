8d0c2caadf5a8890379eb270064ca9108ebcbb98
Connor Clark <Conclarkh5@gmail.com>
2019-12-02 10:43:41 -0800

    edits

--- javascript-tests/javascript-arrays.js
+++ javascript-tests/javascript-arrays.js
@@ -9,18 +9,34 @@ window.onload = () => {
     };
 
     const numbersAction = document.getElementById('numbers-action');
-    const numbersDisplay = document.getElementById('numbers-display');
+    const languageNamesAction = document.getElementById('language-names-action');
+    const techLanguagePopularityAction = document.getElementById('tech-language-popularity-action');
     
     const arrays = {
-        numbers: [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5]
+        numbers: [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5],
+        languageNames: [JavaScript, HTML, CSS, Java, 'C#', Python, 'C/C++'],
     };
 
     arrays.numbers.sort((a, b) => a - b);
 
     numbersAction.onclick = () => {
+        const numbersDisplay = document.getElementById('numbers-display');
         for (let number of arrays.numbers) {
             console.log(number);
             elfCode.appendToList(numbersDisplay, number);
         }
+    };
+
+    languageNamesAction.onclick = () => {
+        const languageNamesDisplay = document.getElementById('language-names-display');
+        for (let value of arrays.languageNames) {
+            console.log(value);
+            elfCode.appendToList(languageNamesDisplay, value);
+        }
+    };
+
+    techLanguagePopularityAction.onclick = () => {
+        const techLanguagePopularityDisplay = document.getElementById('tech-language-popularity-display');
+
     };
 }
