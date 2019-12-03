cafa647f58be95e068f5d1c9ae6960576cfe1e17
Connor Clark <Conclarkh5@gmail.com>
2019-12-02 21:19:09 -0800

    edits

--- javascript-tests/object-choices.js
+++ javascript-tests/object-choices.js
@@ -1,30 +1,36 @@
-const simpleObject = {
-    sayName: function() {
-        console.log('Simple Object');
-    }
+window.onload = () => {
+
+    const simpleObject = {
+        sayName: function() {
+            console.log('Simple Object');
+        }
+    };
+
+    simpleObject.dynamicMethod = () => {
+        console.log('Dynamic Method');
+    };
+
+    function functionObject() {
+        functionObject.prototype.sayName = function() {
+            console.log('Function Object');
+        }
+        function privateFunction() {
+            console.log('Private Function');
+        };
+    };
+
+    class CustomClass {
+        sayName() {
+            console.log('Custom Class');
+        }
+    };
+
+    simpleObject.sayName();
+    simpleObject.dynamicMethod();
+
+    const functionObject = new FunctionObject();
+    functionObject.sayName();
+
+    const customClass = new CustomClass();
+    CustomClass.sayName();
 };
-
-simpleObject.dynamicMethod = () => {
-    console.log('Dynamic Method');
-};
-
-function functionObject() {
-    functionObject.prototype.sayName = function() {
-        console.log('Function Object');
-    }
-};
-
-class CustomClass {
-    sayName() {
-        console.log('Custom Class');
-    }
-};
-
-simpleObject.sayName();
-simpleObject.dynamicMethod();
-
-const functionObject = new FunctionObject();
-functionObject.sayName();
-
-const customeClass = new CustomClass();
-CustomClass.sayName();
